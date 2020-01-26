f1e3e473bcca40cc88af121ae0a8edc93dc516b6
licia03 <lgavina7@gmail.com>
2020-01-26 18:48:06 +0100

    Updates

---
+++ precache-manifest.820e0ddc10bac1ed173c2a1b256f7dc7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06798a3f7ac28be1c4b2811c90c24253",
+    "url": "/react-typescript-search-tech-articles/index.html"
+  },
+  {
+    "revision": "3c1b29049f20f17c2883",
+    "url": "/react-typescript-search-tech-articles/static/css/main.ff720124.chunk.css"
+  },
+  {
+    "revision": "8d139d4a0103e9e84e90",
+    "url": "/react-typescript-search-tech-articles/static/js/2.19c71179.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-typescript-search-tech-articles/static/js/2.19c71179.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3c1b29049f20f17c2883",
+    "url": "/react-typescript-search-tech-articles/static/js/main.435900ae.chunk.js"
+  },
+  {
+    "revision": "21d8ca5781c641cb62ce",
+    "url": "/react-typescript-search-tech-articles/static/js/runtime-main.9533f961.js"
+  }
+]);
